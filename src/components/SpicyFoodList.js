3a8081eed35aa8d8541d1198d49f0afd3840be09
Jessica Nelson <yahwehschica@yahoo.com>
2022-02-22 12:36:26 -0500

    commit tree e98631b98c07b34c3961a1b7c51e68f4f663db04

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,14 +3,18 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All")
 
-  function handleAddFood() {
-    const newFood = getNewSpicyFood();
-    console.log(newFood);
-  }
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  })
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -22,5 +26,44 @@ function SpicyFoodList() {
     </div>
   );
 }
+
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  return (
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
+  )
+
+  
+
+  function handleAddFood() {
+    const newFood = getNewSpicyFood();
+    const newFoodArray = ([...foods, newFood]);
+    setFoods(newFoodArray);
+  }
+
+  function handleLiClick() {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);  
+
+  }
+
+  
 
 export default SpicyFoodList;
